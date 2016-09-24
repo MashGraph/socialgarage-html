f051f21d79a9e8772415f4484188ad08fa3ca38a
Ian Pansensoy <ipansensoy@gmail.com>
2016-09-24 22:32:15 +0800

    animated chat

--- html/app/js/main.js
+++ html/app/js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
   $('#nav-icon1').click(function() {
     $(this).toggleClass('open');
+    $('.chat-user-list').toggleClass('open');
+
   });
+  $('.scroll-chat-list').slimscroll({
+    height: '75%',
+    distance: '3px',
+    size: '3px',
+    railVisible: false
+
+
+
+  });
+  $('.user-choose-file').popover({
+    html: true,
+    content: function() {
+      return $('#popover-content').html();
+    }
+  });
+
 });
