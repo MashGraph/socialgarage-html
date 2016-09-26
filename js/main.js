e83b799fcf87524d93508a5d0f334b5004b16164
Ian Pansensoy <ipansensoy@gmail.com>
2016-09-26 13:49:37 +0800

    post deployment

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-$(document).ready(function(){var l,o=$(window).width();o>=768&&(l="81%"),o>=1024&&(l="75%"),o<=767&&(l="66%"),console.log(l),$("#nav-icon1").click(function(){$(this).toggleClass("open"),$(".chat-user-list").toggleClass("open")}),$(".chat-toggle").click(function(){$(".chat-box").toggleClass("open")}),$(".scroll-chat-list").slimscroll({height:l,distance:"3px",size:"3px",railVisible:!1}),$(".direct-scroll").slimscroll({height:"300px",distance:"3px",size:"3px",railVisible:!1}),$(".user-choose-file").popover({html:!0,content:function(){return $("#popover-content").html()}})});px',
+$(document).ready(function() {
+  var wWidth = $(window).width();
+  var curScrollHeight;
+  if(wWidth >= 768){
+    curScrollHeight = '81%';
+  }
+  if(wWidth >= 1024){
+    curScrollHeight = '75%';
+  }
+  if(wWidth <= 767){
+    curScrollHeight = '66%';
+  }
+
+  console.log(curScrollHeight);
+  $('#nav-icon1').click(function() {
+    $(this).toggleClass('open');
+    $('.chat-user-list').toggleClass('open');
+
+  });
+
+  $('.chat-toggle').click(function() {
+
+    $('.chat-box').toggleClass('open');
+
+  });
+  $('.scroll-chat-list').slimscroll({
+    height: curScrollHeight,
+    distance: '3px',
     size: '3px',
     railVisible: false
 
